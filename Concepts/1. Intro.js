a5005744465ad481efd28d8c1d8b9df93d5f8c06
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-26 00:21:59 +0530

    Update 1. Intro.js

--- Concepts/1. Intro.js
+++ Concepts/1. Intro.js
@@ -52,4 +52,30 @@ so this file will be kind of cheat sheet to refer at any time.
   - git chekout branchname      // it will chekout on latest commit on that branch
   
   
-12. 
+12. to see available branches:-
+	- git branch
+
+
+13. to create branch 
+	- git branch <branch_name>
+
+
+14. to chekout on another branch
+	- git checkout <branch_name>
+
+
+15. to create & checkout on new branch
+	- git checkout -b <branch_name>
+
+
+16. to push branch on remote
+	- git push --set-upstream origin <branch_name>
+
+
+17. to merge branch 
+	- chekout to the branch in which you want to merge another branch (e.g chekout to develop)
+	- git merge <branch_name... feature/myFeature> 
+
+18. to check remote repo
+	- git remote -v
+
