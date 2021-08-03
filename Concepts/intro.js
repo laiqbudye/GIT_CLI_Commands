f7222fbac4291923ac5a5e7dfae709c1902b40bc
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-04 00:15:29 +0530

    Update intro.js

--- Concepts/intro.js
+++ Concepts/intro.js
@@ -1,4 +1,21 @@
 What is GIT?
-  is a distributed version conrol system
+  is a version conrol system which is open source
+  basically it is a software for tracking changes made to files.
   open-source
+
+  main feature of git is 
+    distributed - which means each developer has its own copy.
+
+
+
+package.json
+    contains all data in key value pair.
+    we can say that package.json is the heart of our application. 
+    exist at root level of our application.
+    contains entry point to our application.
+    also we can have scripts & dependencies related to app inside this file.
   
+
+.gitignore
+    it is a normal text file which tells git which files to ignore when commiting a project on github.
+    this is also placed at root directory of app.
